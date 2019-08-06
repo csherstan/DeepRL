b397631f0bfac60f5e2edc4c83638fbfe39423ce
Craig Sherstan <craig.sherstan@borealisai.com>
2019-08-06 16:50:08 -0600

    add weighting of 1000

---
+++ bash/aux_0.99_1000.0.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=24G
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=compute011
+
+source $BASH_CFG
+activate_venv
+
+# run from bash directory
+time python ../aux_0.99_1000.0.py \
+--game BreakoutNoFrameskip-v4 --run $SLURM_ARRAY_TASK_ID
