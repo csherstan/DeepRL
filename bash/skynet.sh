dd4a8ba429e634d215358deccb06a3e4fb397765
Craig Sherstan <craig.sherstan@gmail.com>
2019-09-13 09:17:36 -0600

    remove unused args from skynet.sh

--- bash/skynet.sh
+++ bash/skynet.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-export PROJECT_PATH="/home/sherstan/workspace/return_ae"
 export BASH_CFG=$PROJECT_PATH/return_ae/bash/skynet.sh
-export DATA=/home/sherstan/data
-export WORKSPACE=/home/sherstan/workspace
-export THREE_D_CTRL=${WORKSPACE}/3d_control_deep_rl
+export DATA_DIR=
 
 activate_venv () {
     source activate return_ae
