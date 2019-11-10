119248e41cc2b34947d9f5e1cf08f63b4a39b309
Craig Sherstan <craig.sherstan@gmail.com>
2019-11-09 22:50:18 -0700

    correcting paths for graham

--- bash/graham.sh
+++ bash/graham.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+PROJECT_PATH=/home/sherstan/workspace
 export BASH_CFG=$PROJECT_PATH/DeepRL/bash/graham.sh
-export DATA_DIR=/home/craig/data/deeprl
+export DATA_DIR=/home/sherstan/scratch/deeprl
 
 activate_venv () {
     source /home/sherstan/workspace/venv/deeprl/bin/activate
